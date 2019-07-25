dd275d8c21cc97596fd09f4a452b57af5f7755e5
labohkip81 <labankiplagat81@gmail.com>
2019-07-25 07:55:03 +0300

    Successfully ran migrations and played around with the api or python shell options.

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Questions(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
