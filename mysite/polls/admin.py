6adeb8607d8553e299bea8050c2dd403212015c0
labohkip81 <labankiplagat81@gmail.com>
2019-07-25 08:51:08 +0300

    Solved timezone error.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -3,4 +3,7 @@ from django.contrib import admin
 # Register your models here.
 from .models import Question
 
+#This registers the polls app models to be visible in the admins panel.
 admin.site.register(Question)
+
+
